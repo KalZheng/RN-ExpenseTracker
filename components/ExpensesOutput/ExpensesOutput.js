cfb6b35ddf9602746a9bfc98650c895a628eab63
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-24 15:45:48 +0800

    add expense context and add reducer for modified the state of the array

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,56 +4,6 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "a pair of shoes",
-    amount: 59.99,
-    date: new Date("2021-12-19"),
-  },
-  {
-    id: "e2",
-    description: "some banana",
-    amount: 5.99,
-    date: new Date("2021-01-19"),
-  },
-  {
-    id: "e3",
-    description: "food",
-    amount: 300.0,
-    date: new Date("2021-02-19"),
-  },
-  {
-    id: "e4",
-    description: "a pair of pants",
-    amount: 23.75,
-    date: new Date("2021-02-28"),
-  },
-  {
-    id: "e5",
-    description: "drinks",
-    amount: 4.0,
-    date: new Date("2021-03-14"),
-  },
-  {
-    id: "e6",
-    description: "food",
-    amount: 300.0,
-    date: new Date("2021-02-19"),
-  },
-  {
-    id: "e7",
-    description: "a pair of pants",
-    amount: 23.75,
-    date: new Date("2021-02-28"),
-  },
-  {
-    id: "e8",
-    description: "drinks",
-    amount: 4.0,
-    date: new Date("2021-03-14"),
-  },
-];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -17,11 +17,17 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  function deleteExpenseHandler() {}
+  function deleteExpenseHandler() {
+    navigation.goBack();
+  }
 
-  function cancelHandler() {}
+  function cancelHandler() {
+    navigation.goBack();
+  }
 
-  function confirmHandler() {}
+  function confirmHandler() {
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
