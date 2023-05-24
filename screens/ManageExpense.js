13f5d84e1ac2efb58e7bea971bbf1f237b1e5bc0
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-24 14:39:19 +0800

    dynamic heading for manage expense screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,18 @@
+import { useLayoutEffect } from "react";
 import { View, Text } from "react-native";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+  //the ? check to see if the variable exist/
+  // if not it will pass back undefine and not through an error
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId; //convert value into boolean
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View>
       <Text>Manage Expense</Text>
