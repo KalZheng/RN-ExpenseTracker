95d1f01709cea44b22655e8e8e90dc09b803587c
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-26 16:00:52 +0800

    add get data function

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,11 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpenseContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpense } from "../util/http";
 
 function RecentExpenses() {
   const expensesCtx = useContext(ExpenseContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpense();
+    }
+    getExpenses();
+  }, []);
+
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
     const daysAgo = getDateMinusDays(today, 7);
